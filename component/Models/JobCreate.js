c152cba3cf84d2bba23a93df9ba367e90a05567f
Shintaro0221 <sintaronettt@gmail.comt>
2022-06-05 04:50:55 -0700

    feat

--- component/Models/JobCreate.js
+++ component/Models/JobCreate.js
@@ -1,57 +1,94 @@
+import { useRouter } from "next/router";
 import {
   Box,
-  FormControl,
   FormLabel,
-  Input,
   Button,
   Textarea,
+  Input,
   SimpleGrid,
   Center,
 } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 export const JobCreate = () => {
+  const router = useRouter();
+  const onSubmitHandler = (data) => {
+    console.log({ data });
+    reset();
+    router.push("/");
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   return (
     <>
       <Box borderRadius="md" bg="white" color="white" m={2} p={2}>
-        <FormControl>
-          <FormLabel p={2} color={"black"}>
-            Title
-          </FormLabel>
-          <Input id="title" type="title" />
-          <FormLabel p={2} color={"black"}>
-            Title
-          </FormLabel>
-          <Input id="title" type="title" />
-          <SimpleGrid column={2} minChildWidth="360px">
-            <div mx={2}>
-              <FormLabel p={2} color={"black"}>
-                Location
-              </FormLabel>
-              <Input id="location" type="location" />
-            </div>
-            <div mx={2}>
-              <FormLabel p={2} mx={2} color={"black"}>
-                Salary
-              </FormLabel>
-              <Input id="salary" type="salary" />
-            </div>
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <FormLabel color={"black"}>Title</FormLabel>
+          <Input {...register("title")} id="title" type="title" color="black" />
+          <SimpleGrid column={2} minChildWidth="360px" spacing={4} py={2}>
+            <Box>
+              <FormLabel color={"black"}>Organization</FormLabel>
+              <Input
+                {...register("organization")}
+                id="organization"
+                type="organization"
+                color="black"
+              />
+            </Box>
+            <Box>
+              <FormLabel color={"black"}>Role</FormLabel>
+              <Input
+                {...register("role")}
+                id="role"
+                type="role"
+                color="black"
+              />
+            </Box>
+            <Box>
+              <FormLabel color={"black"}>Location</FormLabel>
+              <Input
+                {...register("location")}
+                id="location"
+                type="location"
+                color="black"
+              />
+            </Box>
+            <Box>
+              <FormLabel color={"black"}>Salary</FormLabel>
+              <Input
+                {...register("salary")}
+                id="salary"
+                type="salary"
+                color="black"
+              />
+            </Box>
           </SimpleGrid>
 
-          <FormLabel p={2} mx={2} color={"black"}>
-            Description
-          </FormLabel>
+          <FormLabel color={"black"}>Description</FormLabel>
           <Textarea
             // value={value}
-            // onChange={handleInputChange}
+            // onChange={handleinputChange}
+            {...register("detail")}
             placeholder="Description placeholder"
             size="md"
+            color={"black"}
           />
-        </FormControl>
-        <Center p={4}>
-          <Button backgroundColor="brandMain" size="lg">
-            Create Job
-          </Button>
-        </Center>
+
+          <Center p={4}>
+            <Button
+              backgroundColor="brandMain"
+              size="lg"
+              type="submit"
+              onClick={() => onSubmitHandler()}
+            >
+              Create Job
+            </Button>
+          </Center>
+        </form>
       </Box>
     </>
   );
