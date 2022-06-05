6400f31f6a3dd8f001e2837ffdff246af8a28920
Shintaro0221 <sintaronettt@gmail.comt>
2022-06-05 05:06:55 -0700

    fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createClient as createUrqlClient } from "urql";
-import { refreshAuthToken } from "../utils";
+// import { refreshAuthToken } from "../utils";
 
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
