56b3ecf4aac15afbcae60272d880b7395a89247f
Shintaro0221 <sintaronettt@gmail.comt>
2022-06-05 00:56:16 -0700

    fix: typo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 import { Navbar } from "../component/Layout/Navbar";
 import { Footer } from "../component/Layout/Footer";
 // import { Navbar, Footer } from "../component/Layout";
-import { Home } from "../component/Pages/Home";
+import { Home } from "../component/Pages/home";
 export default function Index() {
   return (
     <Box>
